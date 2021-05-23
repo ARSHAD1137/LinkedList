d93db1c3c2fee5c45a504340c328f52133509b19
Arshad <arshadpinjari666@gmail.com>
2021-05-23 17:04:39 +0530

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Using Linked List");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+            list.RemoveFirstNode();
+            list.RemovaLastNode();
+            list.Search(56);
+            Console.ReadKey();
+        }
+    }
+}
